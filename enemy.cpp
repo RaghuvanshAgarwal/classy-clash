2e6b7c8b56474c9f5021954ae8855a4b98c6bf0f
Raghuvansh Agarwal <raghu@convai.com>
2025-03-07 18:42:33 +0530

    Initialize WorldPosition in Enemy constructor and update ScreenPosition calculation in Tick method

--- enemy.cpp
+++ enemy.cpp
@@ -5,10 +5,12 @@
 
 Enemy::Enemy(const Texture2D &idle_texture, const Texture2D &run_texture, Vector2 pos) : BaseCharacter(idle_texture, run_texture)
 {
+    WorldPosition = pos;
 }
 
 void Enemy::Tick(float dt)
 {
     LastWorldPosition = WorldPosition;
+    ScreenPosition = Vector2Subtract(WorldPosition, Target->GetWorldPosition());
     BaseCharacter::Tick(dt);
 }
